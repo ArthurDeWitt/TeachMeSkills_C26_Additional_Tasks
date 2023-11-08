3e96bfd0cb8a3fb5ecb3e0b82d9c1d583383dbc5
ArthurDeWitt <124618265+ArthurDeWitt@users.noreply.github.com>
2023-11-08 15:44:26 +0300

    initial commit

---
+++ src/SumDigits.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SumDigits {
+
+    public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            // Ввод числа
+            System.out.println("Enter a number");
+            int number = scanner.nextInt();
+            // Инициализация счётчика
+            int sum = 0;
+            // Ввод переменной сохранения исходного числа
+            int saveNumber = number;
+
+            while (saveNumber != 0){
+                int digit = saveNumber % 10;
+                sum += digit;
+                saveNumber /= 10;
+            }
+            System.out.println("The sum of the digits of a number" + number + "is equal to " + sum);
+        }
+}
