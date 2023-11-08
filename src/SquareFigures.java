78e0c998b5c6b0f4c6127a877a3cc2248ee96e22
ArthurDeWitt <124618265+ArthurDeWitt@users.noreply.github.com>
2023-11-08 15:41:38 +0300

    Problem with enter of negative values

---
+++ src/SquareFigures.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class SquareFigures {
+    public static void main(String[] args) {
+        // Создание переменной для хранения вычислений площади
+        double square = 0;
+        // Создание переменных для хранения длины, ширины, радиуса, сторон треугольника
+        double length, width, radius, side1, side2, side3;
+        // Инициализация числа Пи
+        double pi = Math.PI;
+        // Инициализация ввода кода фигуры
+        byte indexOfFigure;
+        System.out.println("Select code of a figure: Rectangle - 1; Circle - 2; Triangle - 3: ");
+        Scanner scanner = new Scanner(System.in);
+        indexOfFigure = scanner.nextByte();
+
+        switch (indexOfFigure) {
+            // Прямоугольник
+            case 1:
+                // Ввод длины и ширины прямоугольника
+                System.out.println("Enter the length of the rectangle");
+                Scanner scanner1 = new Scanner(System.in);
+                length = scanner1.nextDouble();
+                System.out.println("Enter the width of the rectangle");
+                Scanner scanner2 = new Scanner(System.in);
+                width = scanner2.nextDouble();
+                // Расчёт площади прямоугольника
+                square = length * width;
+                System.out.println("Square of rectangle is: " + square);
+                return;
+            // Круг
+            case 2:
+                // Ввод радиуса круга
+                System.out.println("Enter the value of the circle radius");
+                Scanner scanner3 = new Scanner(System.in);
+                radius = scanner3.nextDouble();
+                square = pi * radius * radius;
+                System.out.println("Square of rectangle is: " + square);
+                return;
+            // Треугольник
+            case 3:
+                // Ввод сторон треугольника
+                System.out.println("Enter the first side of the triangle");
+                Scanner scanner4 = new Scanner(System.in);
+                side1 = scanner4.nextDouble();
+                System.out.println("Enter the second side of the triangle");
+                Scanner scanner5 = new Scanner(System.in);
+                side2 = scanner5.nextDouble();
+                System.out.println("Enter the third side of the triangle");
+                Scanner scanner6 = new Scanner(System.in);
+                side3 = scanner6.nextDouble();
+                /*
+                 Проверка существования такого треугольника
+                 (Если сумма двух любых сторон меньше третьей, то такого треугольника не существует)
+                 */
+                if (side1 + side2 > side3 && side2 + side3 > side1 && side1 + side3 > side2) {
+                    // Инициализация расчёта полупериметра для треугольника
+                    double p = (side1 + side2 + side3) / 2;
+                    // Расчёт площади прямоугольника по формуле Герона
+                    square = Math.sqrt(p * (p - side1) * (p - side2) * (p - side3));
+                    System.out.println("Square of triangle = " + square);
+                } else {
+                    System.out.println("Such a triangle doesn't exist");
+                }
+                return;
+            default:
+                System.out.println("Wrong code of figure");
+        }
+    }
+}
+
